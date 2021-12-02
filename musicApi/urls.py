716c389e90b74ed67e464510c713d6ff6a9c5c37
Prince Igwenagha <Igwep297@gmail.com>
2021-12-02 14:12:16 +0100

    added drf_yasg for schema and documentations

--- musicApi/urls.py
+++ musicApi/urls.py
@@ -20,7 +20,31 @@ from django.conf import settings
 from django.urls.conf import include
 from .api import router
 
+from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Music API",
+        default_version = 'v1',
+        description = "A music api containing genres, albums, and users",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="igwep297@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    # path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/auth/', include('djoser.urls.jwt')),
+    # path("api/auth/", include("accounts.urls")),
+    path("api/auth/", include("djoser.urls")),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

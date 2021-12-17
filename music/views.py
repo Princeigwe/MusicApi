42ad87358aa379d00900901d638e5822f2a37808
Prince Igwenagha <Igwep297@gmail.com>
2021-12-17 14:38:03 +0100

    fixed post request issue

--- music/views.py
+++ music/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Music
+from .serializers import MusicSerializer
+from rest_framework.parsers import MultiPartParser, FileUploadParser
 # Create your views here.
+
+class ListOrCreateMusicAPIView(generics.ListCreateAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+    parser_classes = [MultiPartParser, FileUploadParser]
+
+class RetrieveOrDeleteMusicAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer

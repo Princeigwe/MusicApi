a496eae57447cd1daca4b05546261cf227641fa6
Prince Igwenagha <Igwep297@gmail.com>
2021-12-23 05:07:06 +0100

    added app host to allowed hosts

--- musicApi/settings.py
+++ musicApi/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=0)
 
-ALLOWED_HOSTS = ['0.0.0.0', '.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'music-api-app.herokuapp.com']
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
 
